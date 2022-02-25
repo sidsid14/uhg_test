6257a4aa8bda9fad565b373b906a2e518391ba22
Sudhanshu <sidsid14@github.com>
2022-02-25 14:57:00 +0530

    Custom Validator for matching passwords

---
+++ src/app/auth/validators/match-password.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { AbstractControl, ValidationErrors, Validator } from "@angular/forms";
+
+@Injectable({providedIn:'root'})
+export class MatchPassword implements Validator {
+    validate(control: AbstractControl): ValidationErrors | null {
+            const { password, passwordConfirmation } = control.value;
+        
+            if (password === passwordConfirmation) {
+              return null;
+            } else {
+              return { paswordsDontMatch: true };
+            }
+    }
+}
