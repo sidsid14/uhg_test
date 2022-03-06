4a314f4a80a9a95fc2dd677acad9cbb3a530a759
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-03-06 16:44:52 +0530

    Modal component for email compose

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,11 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ContainerComponent } from './container/container.component';
-
-
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [
-    InputComponent,
-    ContainerComponent
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
-  exports:[InputComponent, ContainerComponent]
+  declarations: [InputComponent, ContainerComponent, ModalComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [InputComponent, ContainerComponent, ModalComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
