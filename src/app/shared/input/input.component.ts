781b527bdcdb334a0e285601c5b0a905e6eccb09
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-03-06 17:35:32 +0530

    Customize reusable input component to offer textarea as well

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -4,20 +4,19 @@ import { AbstractControl, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
-  @Input() label:string = '';
-  @Input() control:FormControl = new FormControl();
-  @Input() inputType:string = 'text';
-  constructor() { }
+  @Input() label: string = '';
+  @Input() control: FormControl = new FormControl();
+  @Input() inputType: string = 'text';
+  @Input() controlType: string = 'input';
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  showErrors(){
-    const {dirty, touched, errors} = this.control;
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
     return (dirty || touched) && errors;
   }
-
 }
